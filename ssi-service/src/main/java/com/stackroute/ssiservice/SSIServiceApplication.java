b9e4b70a226cae3e2a007596d8e86e9e091642e7
Mohit Sharma - IT <mohit.2.sharma@niit.com>
2023-03-10 11:27:06 +0530

    boilerplate added

---
+++ ssi-service/src/main/java/com/stackroute/ssiservice/SSIServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.ssiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSIServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ ssi-service/src/test/java/com/stackroute/ssiservice/SSIServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.ssiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSIServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
