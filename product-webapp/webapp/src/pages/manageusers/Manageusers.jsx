80705cd2d75d23efbe1fca86b920bc1d5197d356
Ajay Mathew <ajay.mathew@batonsystems.com>
2023-03-17 11:04:04 +0530

    reuseable export component is created

--- product-webapp/webapp/src/pages/manageusers/Manageusers.jsx
+++ product-webapp/webapp/src/pages/manageusers/Manageusers.jsx
@@ -2,24 +2,19 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Box, useTheme, TextField, IconButton,Menu,MenuItem } from '@mui/material';
-import { DataGrid, GridToolbar, GridActionsCellItem } from "@mui/x-data-grid";
+import { Box, useTheme, TextField} from '@mui/material';
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
 import { tokens } from "../../themes";
-import "./styles.css";
 
-import jsPDF from 'jspdf'
-import autoTable from 'jspdf-autotable'
 
 import { getUsers, updateUserAdminStatus, removeUser } from "../../services/userservices";
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import SecurityIcon from '@mui/icons-material/Security';
-// import EditIcon from '@mui/icons-material/Edit';
-import DownloadIcon from '@mui/icons-material/Download';
 import PersonIcon from '@mui/icons-material/Person';
 
-
 import Header from '../../components/Header';
+import Export from '../../components/Export/Export'
 
 const Manageusers = () => {
     const theme = useTheme();
@@ -33,6 +28,9 @@ const Manageusers = () => {
     const [rows, setRows] = useState([]);
     const [modifiedRows, setModifiedRows] = useState(rows);
 
+    const clearTextField = (e) => {
+        e.target.value = "";
+    }
 
     const updateRows = () => {
         getUsers().then(res => {
@@ -63,67 +61,7 @@ const Manageusers = () => {
         })
     }
 
-    const clearTextField = (e) => {
-        e.target.value = "";
-    }
-
-    const exportCsv = () => {
-        const csvString = [
-            [
-                "id",
-                "userName",
-                "userEmail",
-                "userCompany",
-                "userRole"
-            ],
-            ...modifiedRows.map(item => [
-                item.id,
-                item.userName,
-                item.userEmail,
-                item.userCompany,
-                item.userRole
-            ])
-        ].map(e => e.join(",")).join("\n");
-        const csv_data = `${csvString}`.split("\n").join('\n');
-
-        let CSVFile = new Blob([csv_data], { type: "text/csv" });
-        var temp_link = document.createElement('a');
-        temp_link.download = document.title;
-        var url = window.URL.createObjectURL(CSVFile);
-        temp_link.href = url;
-        temp_link.style.display = "none";
-        document.body.appendChild(temp_link);
-        temp_link.click();
-        document.body.removeChild(temp_link);
-    }
-
-    const exportPdf = () => {
-        const csvString = [
-            [
-                "id",
-                "userName",
-                "userEmail",
-                "userCompany",
-                "userRole"
-            ],
-            ...modifiedRows.map(item => [
-                item.id,
-                item.userName,
-                item.userEmail,
-                item.userCompany,
-                item.userRole
-            ])
-        ].map(e => e.join(",")).join("\n");
-        const csv_data = `${csvString}`.split("\n");
-        const head = csv_data.shift().split(",")
-        const body = csv_data.map(n => n.split(','))
-        const doc = new jsPDF()
-        autoTable(doc, {
-            head: [head],
-            body: body,
-        });
-        doc.save(document.title);
-    }
+    
 
     const columns = [
         { field: "id", headerName: "ID" },
@@ -163,41 +101,8 @@ const Manageusers = () => {
         updateRows();
     }, []);
 
-    const dummyMenuItems = [
-        {
-            title: "Export as PDF",
-            id:"pdf"
-        },
-        {
-            title: "Export as CSV",
-            id:"csv"
-        }
-    ];
-
-    const [anchorEl, setAnchorEl] = useState(null);
-    const handleClick = e => {
-        setAnchorEl(e.currentTarget);
-    };
-    const handleClose = (e) => {
-        setAnchorEl(null);
-        if(e.target.id==='pdf'){
-            exportPdf()
-        }else if(e.target.id==='csv'){
-            exportCsv();
-        }
-    };
-    const nativeOnChange = e => {
-        const detail = {
-            selectedIndex: e.target.selectedIndex
-        };
-        e.target.selectedIndex = 0;
-
-        e.target.dispatchEvent(new CustomEvent("itemClick", { detail }));
-    };
-
-    const itemClick = e => {
-        console.log("Item Clicked " + e.detail);
-    };
+   
+    
 
     return (
         <Box m='0 20px'>
@@ -288,26 +193,7 @@ const Manageusers = () => {
                                 variant="standard"
                             />
                         </Box>
-                        <Box>
-                            <IconButton aria-controls="simple-menu"
-                                aria-haspopup="true"
-                                onClick={handleClick}
-                                aria-label="Open to show more"
-                                title="Open to show more"><DownloadIcon /></IconButton>
-                            <Menu
-                                id="simple-menu"
-                                anchorEl={anchorEl}
-                                keepMounted
-                                open={Boolean(anchorEl)}
-                                onClose={handleClose}
-                            >
-                                {dummyMenuItems.map(item => (
-                                    <MenuItem onClick={handleClose} key={item.title} id={item.id} value={item.title}>
-                                        {item.title}
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
+                        <Export modifiedRows/>
                     </Box>
                     <DataGrid
                         rows={modifiedRows}
