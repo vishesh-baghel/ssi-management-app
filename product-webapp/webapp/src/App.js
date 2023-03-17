5123e8508c23fd5f8539b02712781096093ab088
vishesh-baghel <visheshbaghel99@gmail.com>
2023-03-17 10:41:50 +0530

    fix: routing bug fixed

--- product-webapp/webapp/src/App.js
+++ product-webapp/webapp/src/App.js
@@ -15,17 +15,17 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Appwrapper />}>
+      <Route path='/dashboard' element={<Appwrapper />}>
               <Route index element={<Dashboard />} />
-              <Route path="/manageusers" element={<Manageusers />} />
-              <Route path="/managessi" element={<Managessi />} />
-              <Route path="/addssi" element={<Addssi />} />
-              <Route path="/adduser" element={<Adduser />} />
-              <Route path="/ssi/:id" element={<Viewssi/>}/>
+              <Route path="manageusers" element={<Manageusers />} />
+              <Route path="managessi" element={<Managessi />} />
+              <Route path="addssi" element={<Addssi />} />
+              <Route path="adduser" element={<Adduser />} />
+              <Route path="ssi/:id" element={<Viewssi/>}/>
       </Route>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
-      <Route path='/home' element={<Landing />} />
+      <Route path='/' element={<Landing />} />
       <Route path='/forgotpassword' element={<ForgotPassword />} />
     </Routes>
     </>
