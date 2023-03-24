b6a81e972273e8364634f0cd78a232db4f8755f8
annmary <annmary.shybu@batonsystems.com>
2023-03-24 18:23:58 +0545

    feat: email service completed

--- email-service/src/main/java/com/stackroute/controller/EmailController.java
+++ email-service/src/main/java/com/stackroute/controller/EmailController.java
@@ -8,10 +8,12 @@ import com.stackroute.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // Annotation
 @RestController
+@RequestMapping("/email")
 // Class
 public class EmailController {
 
