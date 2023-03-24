0c4de33945ab4af46b320cb5c72d255269d16b95
annmary <annmary.shybu@batonsystems.com>
2023-03-24 18:23:58 +0545

    feat: email service completed

--- api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
@@ -7,9 +7,11 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableEurekaClient
+@CrossOrigin
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -27,6 +29,8 @@ public class ApiGatewayApplication {
 				.route(r->r.path("/ssi/**")
 						.uri("lb://SSI-SERVICE"))
 
+				.route(r->r.path("/email/**")
+						.uri("lb://EMAIL-SERVICE"))
 				.build();
 	}
 
